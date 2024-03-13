f062a8d655bb9cf88bcfa1de953ad3249ee9c90b
Mahaba Salim <129070783+mahaba254002@users.noreply.github.com>
2024-03-13 12:54:47 +0800

    added

---
+++ del.py
@@ -0,0 +1,8 @@
+# del is a keyword in python used to delete objects or elements
+# It  differs from pop() because this returns a value.
+x=45
+del(x)
+
+my_list = [1, 2, 3, 4, 5]
+del my_list[2]  # Removes element at index 2 (value 3)
+del my_list[1:3]  # Removes elements at index 1 and 2 (values 2 and 4)
